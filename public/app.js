539928113e741d7aa7f4264e37b3d9ef70ddb5e6
Momo <moharryg@gmail.com>
2026-07-27 20:07:04 +0100

    very bassic setup for emits

--- public/app.js
+++ public/app.js
@@ -1,2 +1,22 @@
 const socket = io();
 
+document.getElementById('sendButton').addEventListener('click', sendMessage);
+
+function sendMessage(event) {
+  event.preventDefault();
+  const input = document.getElementById('messageInput');
+  if (input.value) {
+    socket.emit('chatmessage', input.value);
+    input.value = '';
+  }
+  input.focus();
+};
+
+socket.on('chatmessage', (data) => {
+  console.log(`Message received: ${data}`);
+  const messagesList = document.getElementById('messages');
+  const newMessage = document.createElement('li');
+  newMessage.textContent = data;
+  messagesList.appendChild(newMessage);
+  window.scrollTo(0, document.body.scrollHeight);
+});
--- server.js
+++ server.js
@@ -17,6 +17,20 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+io.on('connection', (socket) => {
+  console.log(`A user connected: ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    console.log(`A user disconnected: ${socket.id}`);
+  });
+  
+  socket.on('chatmessage', (msg) => {
+    console.log(`Message received: ${msg}`);
+    io.emit('chatmessage', msg);
+  });
+  
+});
+
 server.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
